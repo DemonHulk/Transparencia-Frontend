d91978a3a45c2ddd64700eb69d8c170e3eefc311
Rivexis <alexismaindraven@gmail.com>
2024-05-31 22:34:12 -0700

    pdf_mostrar_clic
    que se muestre la informacion al darle clic

--- src/app/components/articulo33/contenido-punto/contenido-punto.component.ts
+++ src/app/components/articulo33/contenido-punto/contenido-punto.component.ts
@@ -10,4 +10,6 @@ export class ContenidoPuntoComponent {
   get shortTitle(): string {
     return this.title.length > 20 ? this.title.substring(0,20) + '...' : this.title;
   }
+
+  descripcion: string = 'El Manual Gubernamental de Contabilidad (UTC) es una guía exhaustiva destinada a estandarizar y mejorar los  procesos contables dentro de las entidades gubernamentales. Este manual establece un conjunto uniforme de principios, normas y procedimientos contables que deben seguir las instituciones públicas para garantizar la transparencia, eficiencia y precisión en la gestión financiera.';
 }
