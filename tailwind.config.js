b285e5a27eaf5da34793939fa67703ebd62aa525
Marco Fabian <al05-016-0321@utdelacosta.edu.mx>
2024-05-31 18:41:05 -0700

    Instalación y configuracion de Preline(Tailwind)
    La libreria CSS que se usara en el proyecto y su implementación (Configuración Básica)

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}",
+  'node_modules/preline/dist/*.js',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary-color': '#077c01',
+        'primary-color-50': '#077c0180',
+        'primary-color-90': '#077c01e6',
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('preline/plugin')
+  ],
+}
