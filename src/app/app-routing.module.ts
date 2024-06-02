7be3828575aa3c51a8d0fa1883c6b9a25e4ed12a
Marco Fabian <al05-016-0321@utdelacosta.edu.mx>
2024-06-02 08:25:53 -0700

    Formulario para registrar un nuevo punto
    El  template para el formulario para registrar un nuevo punto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './components/login/login.component';
 import { Articulo33Component } from './components/articulo33/articulo33.component';
 import { NewPdfComponent } from './components/forms/new-pdf/new-pdf.component';
 import { PageNotFoundComponent } from './partials/page-not-found/page-not-found.component';
+import { NewPuntoComponent } from './components/forms/new-punto/new-punto.component';
 
 const routes: Routes = [
   {path: '', component:Articulo33Component},
   {path: 'login', component:LoginComponent},
   {path: 'articulo33', component:Articulo33Component},
   {path: 'new-pdf', component:NewPdfComponent},
+  {path: 'new-punto', component:NewPuntoComponent},
   {path: '**', component:PageNotFoundComponent},
 ];
 
---
+++ src/app/components/forms/new-punto/new-punto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-punto',
+  templateUrl: './new-punto.component.html',
+  styleUrl: './new-punto.component.css'
+})
+export class NewPuntoComponent {
+
+}
