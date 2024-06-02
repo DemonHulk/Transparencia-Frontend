a868aa6e4aa2ce91e3ecd03743bb64e1f0951602
FabianGomezB <139259893+FabianGomezB@users.noreply.github.com>
2024-06-01 19:37:14 -0700

    Frontend formulario archivo (#2)
    * Formulario para registrar un nuevo PDF
    
    El template para el formulario para registrar un nuevo PDF
    
    * simbolo_interrogacion_inputs
    
    agregado de simbolo de interrogacion a los inptus
    
    * Corrección de ortografias (Parche)
    
    Se resolvio los errores de ortografia
    
    ---------
    
    Co-authored-by: Rivexis <alexismaindraven@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { Articulo33Component } from './components/articulo33/articulo33.component';
+import { NewPdfComponent } from './components/forms/new-pdf/new-pdf.component';
 
 const routes: Routes = [
   {path: 'login', component:LoginComponent},
   {path: 'articulo33', component:Articulo33Component},
+  {path: 'new-pdf', component:NewPdfComponent},
 ];
 
 @NgModule({
