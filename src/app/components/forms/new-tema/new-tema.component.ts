3abd4e1b2d72d23bcd3d8ef93ca1bcaf64599db8
Marco Fabian <al05-016-0321@utdelacosta.edu.mx>
2024-06-20 10:40:11 -0700

    Avance details
    El avance de la informacion

--- src/app/components/forms/new-tema/new-tema.component.ts
+++ src/app/components/forms/new-tema/new-tema.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { SharedValuesService } from '../../../services/shared-values.service';
+import { PuntocrudService } from '../../../services/crud/puntocrud.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CryptoServiceService } from '../../../services/cryptoService/crypto-service.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { validarTextoNormal } from '../../../services/api-config';
 
 @Component({
   selector: 'app-new-tema',
@@ -7,21 +12,80 @@ import { SharedValuesService } from '../../../services/shared-values.service';
   styleUrl: './new-tema.component.css'
 })
 export class NewTemaComponent {
-  constructor(private sharedService: SharedValuesService) { }
+  id: any;
+  idEncrypt: any;
+  nombrePunto: any;
+  formAltaTema: FormGroup;
+
+  constructor(private sharedService: SharedValuesService,
+    private activateRoute: ActivatedRoute,
+    public formulario: FormBuilder,
+    private encodeService: CryptoServiceService,
+    private router: Router,
+    private PuntocrudService: PuntocrudService,
+  ) {
+    this.formAltaTema = this.formulario.group({
+      nombre_titulo: ['',
+        [
+          validarTextoNormal(), // Aplica el validador personalizado, el true significa que no debe estar vacio
+          Validators.required,
+          Validators.minLength(10),
+          Validators.maxLength(70)
+        ],
+      ],
+      tipo_contenido: ['1',
+        [
+          Validators.required,
+        ],
+      ],
+      es_link: ['',
+        [
+          Validators.required,
+        ],
+      ],
+    });
+  }
 
   /**
  * Inicializa el componente y establece el título en el servicio de valores compartidos.
  *
  * @returns {void}
  */
-ngOnInit(): void {
+  ngOnInit(): void {
     /**
      * Llama al método changeTitle del servicio de valores compartidos para actualizar el título.
      *
      * @param {string} newTitle - El nuevo título a establecer.
      * @memberof SharedValuesService
      */
-    this.sharedService.changeTitle('Registrar un nuevo tema para el punto: Nombre punto');
-    this.sharedService.loadScript("/assets/js/validations.js");
-}
+
+    //Tomas la id de la URL
+    this.id = this.activateRoute.snapshot.paramMap.get("punto");
+    this.idEncrypt = this.id;
+
+    //Desencriptar la ID
+    this.id = this.encodeService.decodeID(this.id);
+
+    //Verificar si la ID es null, si es así, redirige a la página de áreas
+    if (this.id === null) {
+      this.router.navigateByUrl("/puntos");
+    }
+    this.GetOnePuntoService(this.id);
+
+    this.sharedService.changeTitle('Registrar un nuevo tema para el punto: ' + this.nombrePunto);
+  }
+
+
+  /** EXTRAE EL NOMBRE DEL PUNTO ACTUAL EN REVISION */
+  GetOnePuntoService(id: any) {
+    const encryptedID = this.encodeService.encryptData(JSON.stringify(this.id));
+    this.PuntocrudService.GetOnePuntoService(encryptedID).subscribe(respuesta => {
+      if (respuesta) { // Verificar si respuesta no es undefined
+        this.nombrePunto = this.encodeService.decryptData(respuesta).resultado?.data?.nombre_punto;
+        this.sharedService.changeTitle('Registrar un nuevo tema para el punto: ' + this.nombrePunto);
+      } else {
+      }
+    }, error => {
+    });
+  }
 }
--- src/app/components/tables/list-puntos/details-punto/details-punto.component.ts
+++ src/app/components/tables/list-puntos/details-punto/details-punto.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { SharedValuesService } from '../../../../services/shared-values.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CryptoServiceService } from '../../../../services/cryptoService/crypto-service.service';
+import { PuntocrudService } from '../../../../services/crud/puntocrud.service';
+import { FechaService } from '../../../../services/format/fecha.service';
 
 @Component({
   selector: 'app-details-punto',
@@ -7,7 +11,21 @@ import { SharedValuesService } from '../../../../services/shared-values.service'
   styleUrl: './details-punto.component.css'
 })
 export class DetailsPuntoComponent {
-  constructor(private sharedService: SharedValuesService) { }
+  id: any;
+  idEncrypt: any;
+  punto: any;
+  constructor(private sharedService: SharedValuesService,
+    private activateRoute: ActivatedRoute,
+    private encodeService: CryptoServiceService,
+    private router: Router,
+    private PuntocrudService:PuntocrudService,
+    private FechaService: FechaService,
+
+
+
+
+
+  ) { }
 
   /**
  * Inicializa el componente y establece el título en el servicio de valores compartidos.
@@ -22,7 +40,33 @@ ngOnInit(): void {
      * @memberof SharedValuesService
      */
     this.sharedService.changeTitle('Información detallada del punto: Normativas de la institución');
+    //Tomas la id de la URL
+    this.id = this.activateRoute.snapshot.paramMap.get("id");
+    this.idEncrypt = this.id;
+
+    //Desencriptar la ID
+    this.id = this.encodeService.decodeID(this.id);
+
+    //Verificar si la ID es null, si es así, redirige a la página de áreas
+    if (this.id === null) {
+      this.router.navigateByUrl("/puntos");
+    }
+
+
+    this.GetOnePuntoService(this.id);
 }
+
+  /** EXTRAE EL NOMBRE DEL PUNTO ACTUAL EN REVISION */
+  GetOnePuntoService(id: any) {
+    const encryptedID = this.encodeService.encryptData(JSON.stringify(this.id));
+    this.PuntocrudService.GetOnePuntoService(encryptedID).subscribe(respuesta => {
+      if (respuesta) { // Verificar si respuesta no es undefined
+        this.punto = this.encodeService.decryptData(respuesta).resultado?.data;
+      } else {
+      }
+    }, error => {
+    });
+  }
 
 getNumbers(): number[] {
   const numbers: number[] = [];
@@ -38,5 +82,12 @@ getNumbersSubtema(): number[] {
     numbers.push(i);
   }
   return numbers;
+}
+
+FormatearDate(fecha: any) {
+  if (!fecha) {
+    return ''; // O cualquier valor predeterminado que prefieras
+  }
+  return this.FechaService.formatDate(fecha);
 }
 }
