8024571f8d5f3582b88c0657b2dfcef649469ba0
Marco Fabian <al05-016-0321@utdelacosta.edu.mx>
2024-07-01 17:40:57 -0700

    Verificacion
    Verificacion cada dos horas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ declare global {
 })
 export class AppComponent {
   dataUsuario: any;
-  private readonly SESSION_TIMEOUT = 60 * 1000; // 1 minuto en milisegundos
+  private readonly SESSION_TIMEOUT = 2 * 60* 60 * 1000; // 2 horas
 
   constructor(
     private router: Router,
@@ -45,26 +45,20 @@ export class AppComponent {
     const lastActivity = localStorage.getItem('lastPageLoad');
     const now = new Date().getTime();
 
-    console.log('Última actividad:', new Date(parseInt(lastActivity || '0')).toLocaleString());
-    console.log('Hora actual:', new Date(now).toLocaleString());
 
     if (this.dataUsuario != null) {
       if (!lastActivity || now - parseInt(lastActivity) > this.SESSION_TIMEOUT) {
-        console.log('Han pasado más de 1 minuto desde la última actividad');
         this.verifySesion(this.dataUsuario.id_usuario, this.dataUsuario.id_area);
       } else {
-        console.log('Sesión activa');
         this.updateLastActivity();
       }
     } else {
-      console.log('No hay datos de usuario');
     }
   }
 
   updateLastActivity() {
     const now = new Date().getTime();
     localStorage.setItem('lastPageLoad', now.toString());
-    console.log('Última actividad actualizada:', new Date(now).toLocaleString());
   }
 
   @HostListener('window:beforeunload', ['$event'])
@@ -73,7 +67,6 @@ export class AppComponent {
   }
 
   verifySesion(usuarioId: any, areaId: any) {
-    console.log('Verificando sesión');
     const data = { usuario: usuarioId, area: areaId };
     const encryptedData = this.encodeService.encryptData(JSON.stringify(data));
 
@@ -87,28 +80,23 @@ export class AppComponent {
               dataUsuario.sesion = true;
               const encryptedUser = CryptoJS.AES.encrypt(JSON.stringify(dataUsuario), 'UZ4"(fa$P9g4ñ').toString();
               localStorage.setItem('user', encryptedUser);
-              console.log('Sesión verificada y actualizada');
               this.updateLastActivity();
             } else {
-              console.log('La sesión no es válida');
               this.cerrarSesion();
             }
           } catch (e) {
-            console.error('Error al desencriptar la respuesta', e);
             this.cerrarSesion();
           }
         } else {
         }
       },
       error: (error) => {
-        console.error('Error en la verificación de sesión', error);
         this.cerrarSesion();
       }
     });
   }
 
   cerrarSesion() {
-    console.log('Cerrando sesión');
     localStorage.removeItem('user');
     localStorage.removeItem('lastPageLoad');
      window.location.href = '/articulo33';
